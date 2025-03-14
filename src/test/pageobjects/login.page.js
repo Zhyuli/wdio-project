e67443c01e3ce807e49a412a81e7f6f8029cc8f1
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2025-03-14 17:00:22 +0200

    Clean up commented-out code for better readability

--- src/test/pageobjects/login.page.js
+++ src/test/pageobjects/login.page.js
@@ -1,13 +1,9 @@
 const { $ } = require("@wdio/globals");
 const Page = require("./page");
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-  /**
-   * define selectors using getter methods
-   */
+  // define selectors using getter methods
+
   get inputUsername() {
     return $("//input[@id='user-name']");
   }
@@ -28,33 +24,10 @@ class LoginPage extends Page {
     await this.btnSubmit.click();
   }
 
-//Opens the login page
+  //Opens the login page
   async open() {
     await super.open("");
   }
-  
-// //Clicks the submit button
-//   async submit() {
-//     await this.btnSubmit.click();
-//   }
-
-//   //Clears the specified input field
-//   //@param {string} field - "username" or "password"
-   
-//   async clearField(field) {
-//     if (field === "username") {
-//       await this.inputUsername.clearValue();
-//     } else if (field === "password") {
-//       await this.inputPassword.clearValue();
-//     } else {
-//       throw new Error(`Invalid field: ${field}. Use "username" or "password".`);
-//     }
-//   }
-// //Clears both username and password fields
-//   async clearFields() {
-//     await this.clearField("username");
-//     await this.clearField("password");
-//   }
 }
 
 module.exports = new LoginPage();
